8514501265e2341f17bbea1dc2513be01e00770c
David Salmon <salmon.david44@gmail.com>
2015-11-06 16:57:02 +0100

    edit game.js

--- js/game.js
+++ js/game.js
@@ -30,13 +30,13 @@ function Case (){
 
 Game.prototype.checkResult = function(){
 	if((this.case[0].status=this.currentPlayer && this.case[1].status=this.currentPlayer && this.case[2].status=this.currentPlayer)
-		|| (this.case[3].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[5].status=this.currentPlayer)
-		|| (this.case[6].status=this.currentPlayer && this.case[7].status=this.currentPlayer && this.case[8].status=this.currentPlayer)
-		|| (this.case[1].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[7].status=this.currentPlayer)
-		|| (this.case[2].status=this.currentPlayer && this.case[5].status=this.currentPlayer && this.case[8].status=this.currentPlayer)
-		|| (this.case[0].status=this.currentPlayer && this.case[3].status=this.currentPlayer && this.case[6].status=this.currentPlayer)
-		|| (this.case[0].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[8].status=this.currentPlayer)
-		|| (this.case[6].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[2].status=this.currentPlayer)){
+		||(this.case[3].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[5].status=this.currentPlayer)
+		||(this.case[6].status=this.currentPlayer && this.case[7].status=this.currentPlayer && this.case[8].status=this.currentPlayer)
+		||(this.case[1].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[7].status=this.currentPlayer)
+		||(this.case[2].status=this.currentPlayer && this.case[5].status=this.currentPlayer && this.case[8].status=this.currentPlayer)
+		||(this.case[0].status=this.currentPlayer && this.case[3].status=this.currentPlayer && this.case[6].status=this.currentPlayer)
+		||(this.case[0].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[8].status=this.currentPlayer)
+		||(this.case[6].status=this.currentPlayer && this.case[4].status=this.currentPlayer && this.case[2].status=this.currentPlayer)){
 
 		alert(this.currentPlayer + " Remporte la partie");
 		return true;
