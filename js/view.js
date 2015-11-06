b5470107cb5bf5f07b600750dc1400f28b122fee
BlackYoup <a.lefebvre@outlook.fr>
2015-11-06 16:39:26 +0100

    Implement Game

--- js/view.js
+++ js/view.js
@@ -3,23 +3,19 @@ window.onload = function(){
 };
 
 function View(){
-
+  this.Game = new Game();
 }
 
 View.prototype.init = function(){
-  var cases = [];
-  for(var i = 0; i < 10; i++){
-    cases.push({});
-  }
-
-  this.displayCases(cases);
+  this.Game.initGame();
+  this.displayCases(this.Game.getCases());
 };
 
 View.prototype.displayCases = function(cases){
   var morpion = document.querySelector('#morpion');
   morpion.innerHTML = '';
 
-  for(var c = 0, line = 0, total = 0; total < 9; c++, total++){
+  for(var c = 0, line = 0, total = 0, length = cases.length; total < length; c++, total++){
     if(c > 2){
       line++;
       c = 0;
@@ -36,6 +32,7 @@ View.prototype.displayCases = function(cases){
     var caseHTML = document.createElement('div');
     caseHTML.className += "case";
     caseHTML.setAttribute("case-id", total);
+    caseHTML.setAttribute("case-type", cases[total].status);
     currentLine.appendChild(caseHTML);
   }
 };
